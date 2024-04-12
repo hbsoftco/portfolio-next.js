e45a707b87654f8f48e8cf8199a8cf3d1b76e164
hossein <hossein.bajan@gmail.com>
2024-04-12 11:12:15 +0330

    Add login and register component

---
+++ src/app/(auth)/auth/login/page.tsx
@@ -0,0 +1,7 @@
+const Login = async () => {
+    return <main className="m-auto">
+      this is login page
+    </main>;
+  };
+  
+  export default Login;
---
+++ src/app/(auth)/auth/register/page.tsx
@@ -0,0 +1,7 @@
+const Register = async () => {
+    return <main className="m-auto">
+      this is register page
+    </main>;
+  };
+  
+  export default Register;
